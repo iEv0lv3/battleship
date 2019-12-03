bdc9953fa7d358201de1d935a903dfccb26f9946
Jordan <jordan.wallace.williams@gmail.com>
2019-12-02 22:15:51 -0700

    Add logic for ship class name, length, and health

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,3 +1,14 @@
 class Ship
+  attr_reader :name, :length
+  attr_accessor :healh
 
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @healh = nil
+  end
+
+  def health
+    @healh = @length
+  end
 end
