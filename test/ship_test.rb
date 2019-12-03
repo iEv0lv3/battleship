e221e26d79a9b1a84c3ca86c12f7728da4271bcc
Jordan <jordan.wallace.williams@gmail.com>
2019-12-02 22:19:27 -0700

    Update ship test cruiser strings

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -7,15 +7,15 @@ require 'pry'
 
 class ShipTest < Minitest::Test
   def test_ship_exists
-    cruiser = Ship.new(‘Cruiser’, 3)
+    cruiser = Ship.new('Cruiser', 3)
 
     assert_instance_of Ship, cruiser
   end
 
   def test_ship_has_length_and_length_and_health
-    cruiser = Ship.new(‘Cruiser’, 3)
+    cruiser = Ship.new('Cruiser', 3)
 
-    assert_equal ‘Cruiser’, cruiser.name
+    assert_equal 'Cruiser', cruiser.name
     assert_equal 3, cruiser.length
     assert_equal 3, cruiser.health
   end
