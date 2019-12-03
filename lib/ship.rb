9cfad730f5c62939a8ba0ff1d20334081a3822c4
Jordan <jordan.wallace.williams@gmail.com>
2019-12-02 22:13:50 -0700

    Prepare for branch

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,14 +1,3 @@
 class Ship
-  attr_reader :name, :length
-  attr_accessor :healh
 
-  def initialize(name, length)
-    @name = name
-    @length = length
-    @healh = nil
-  end
-
-  def health
-    @healh = @length
-  end
 end
