6c0c0d0f9d4d132c17378a731cee4da4791cf5c0
Jenny Klich <jennyklich@gmail.com>
2019-12-04 11:40:29 -0700

    Setup board test

---
+++ test/board_test.rb
@@ -0,0 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require 'pry'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+
+end
