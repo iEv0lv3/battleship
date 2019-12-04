3f5a98215153e0a11b28f6a6f3398a4bfb539e2e
Jenny Klich <jennyklich@gmail.com>
2019-12-03 20:15:48 -0700

    Add and update fired upon cell and ship tests

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -27,5 +27,23 @@ class CellTest < Minitest::Test
     assert_instance_of Ship, cell.ship
   end
 
+  def test_cell_has_fired_upon_method_in_instance_variable
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal false, cell.fired_upon?
+  end
+
+  def test_fire_upon_cell_and_ship
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    cell.fire_upon
+    assert_equal true, cell.fired_upon?
+    assert_equal 2, cell.ship.health
+    2.times do
+      cell.fire_upon
+    end
+    assert_equal true, cell.ship.sunk?
+  end
 
 end
