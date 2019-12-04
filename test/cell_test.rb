fda4a679085537aa00b90096d65a9dfcb48d5771
Jenny Klich <jennyklich@gmail.com>
2019-12-03 22:16:55 -0700

    Added tests to test the render method for hits on ships and output

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -46,4 +46,46 @@ class CellTest < Minitest::Test
     assert_equal true, cell.ship.sunk?
   end
 
+  def test_cell_has_not_been_fired_upon
+    cell_1 = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal ".", cell_1.render
+  end
+
+  def test_cell_has_been_fired_upon_and_no_ship
+    cell_1 = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_1.fire_upon
+    assert_equal "M", cell_1.render
+  end
+
+  def test_cell_fired_upon_and_has_ship
+    cell_1 = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_1.place_ship(cruiser)
+    cell_1.fire_upon
+    assert_equal "H", cell_1.render
+  end
+
+  def test_reveal_ship_in_cell_if_not_fired_upon
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+
+    assert_equal "S", cell_2.render(true)
+  end
+
+  def test_ship_can_be_sunk_and_render_x
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    cell_2.fire_upon
+    assert_equal "H", cell_2.render
+    assert_equal false, cruiser.sunk?
+    2.times do
+      cruiser.hit
+    end
+    assert_equal true, cruiser.sunk?
+    assert_equal "X", cell_2.render
+  end
 end
