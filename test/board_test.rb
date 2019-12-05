143dfbfe8f3019baee0ad0047a76e3c1ae37ee42
Jordan <jordan.wallace.williams@gmail.com>
2019-12-04 22:49:02 -0700

    Add test for valid horizontal ship placement for expected false

--- test/board_test.rb
+++ test/board_test.rb
@@ -8,13 +8,12 @@ require './lib/board'
 require './lib/cell'
 
 class BoardTest < Minitest::Test
-
   def test_if_board_exists
     board = Board.new
     assert_instance_of Board, board
   end
 
-  def test_board_has_16_cells_and_cell_data_accurate
+  def test_board_has_16_cells_and_cell_data_is_accurate
     board = Board.new
     assert_equal "A1", board.cells["A1"].coordinate
     assert_equal "B2", board.cells["B2"].coordinate
@@ -32,7 +31,7 @@ class BoardTest < Minitest::Test
     assert_equal false, board.valid_coordinate?("A22")
   end
 
-  def test_valid_ship_placement_false
+  def test_valid_ship_placement_coordinates_equal_ship_length
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -43,7 +42,8 @@ class BoardTest < Minitest::Test
   def test_valid_ship_placement_true
     board = Board.new
     carrier = Ship.new("Carrier", 3)
+    # binding.pry
     assert_equal true, board.valid_placement?(carrier, ["A1", "A2", "A3"])
+    assert_equal false, board.valid_placement?(carrier, ["A1", "A2", "A4"])
   end
-
 end
