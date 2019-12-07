56da3bd88392c55a1a35d1c96c65f3c80bbc2eca
Jordan <jordan.wallace.williams@gmail.com>
2019-12-07 16:27:50 -0700

    Add game class

---
+++ lib/game.rb
@@ -0,0 +1,13 @@
+class Game
+  attr_reader :human_user, :computer_user
+
+  def initialize
+    @human_user = User.new
+    @computer_user = User.new
+  end
+  def start
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
+  end
+
+end
