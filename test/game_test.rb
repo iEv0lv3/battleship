bebd1d190770d37712c420b9b7cb4a9f9795b391
Jordan <jordan.wallace.williams@gmail.com>
2019-12-09 14:43:31 -0700

    Remove and update game tests

--- test/game_test.rb
+++ test/game_test.rb
@@ -17,23 +17,10 @@ class GameTest < Minitest::Test
     assert_instance_of Game, game1
   end
 
-  def test_can_start_game_menu_screen
-    skip
-    game1 = Game.new
-
-    assert_output "Welcome to BATTLESHIP\nEnter p to play. Enter q to quit.",
-                  game1.start
-  end
-
   def test_game_attributes
     game1 = Game.new
-
+    binding.pry
     assert_instance_of User, game1.human_user
     assert_instance_of User, game1.computer_user
-  end
-
-  def test_user_can_place_ships
-    game1 = Game.new
-    binding.pry
   end
 end
