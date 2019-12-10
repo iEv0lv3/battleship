71ee411741e6116d7a2ac77794404d01f25e19b3
Jordan <jordan.wallace.williams@gmail.com>
2019-12-09 22:49:47 -0700

    Update take turn method and syntax

--- lib/game.rb
+++ lib/game.rb
@@ -15,7 +15,7 @@ class Game
       puts "Goodbye."
     elsif choice == 'p'
       turn = Turn.new(@turn_number, @computer_user, @human_user)
-      turn.cpu_turn_0
+      turn.cpu_first_turn
       puts "I have laid out my ships on the grid."
       human_game_setup
     end
@@ -23,15 +23,19 @@ class Game
 
   def human_game_setup
     turn = Turn.new(@turn_number, @human_user, @computer_user)
-    turn.human_turn_0
+    turn.human_first_turn
     @turn_number += 1
   end
 
   def take_turn
     if @turn_number.odd?
       new_turn = Turn.new(@turn_number, @human_user, @computer_user)
+      new_turn.human_turn
+      @turn_number += 1
     else
       new_turn = Turn.new(@turn_number, @computer_user, @human_user)
+      new_turn.cpu_turn
+      @turn_number += 1
     end
   end
 end
